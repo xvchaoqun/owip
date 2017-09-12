72b38478c54c1b9b7933d1c80c23dabfe7ade20c
lm_laptop <jxliaom@foxmail.com>
2017-09-12 22:36:57 +0800

    update

--- src/main/java/domain/pcs/PcsPrRecommendExample.java
+++ src/main/java/domain/pcs/PcsPrRecommendExample.java
@@ -2,7 +2,6 @@ package domain.pcs;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 public class PcsPrRecommendExample {
@@ -104,32 +103,6 @@ public class PcsPrRecommendExample {
                 throw new RuntimeException("Between values for " + property + " cannot be null");
             }
             criteria.add(new Criterion(condition, value1, value2));
-        }
-
-        protected void addCriterionForJDBCDate(String condition, Date value, String property) {
-            if (value == null) {
-                throw new RuntimeException("Value for " + property + " cannot be null");
-            }
-            addCriterion(condition, new java.sql.Date(value.getTime()), property);
-        }
-
-        protected void addCriterionForJDBCDate(String condition, List<Date> values, String property) {
-            if (values == null || values.size() == 0) {
-                throw new RuntimeException("Value list for " + property + " cannot be null or empty");
-            }
-            List<java.sql.Date> dateList = new ArrayList<java.sql.Date>();
-            Iterator<Date> iter = values.iterator();
-            while (iter.hasNext()) {
-                dateList.add(new java.sql.Date(iter.next().getTime()));
-            }
-            addCriterion(condition, dateList, property);
-        }
-
-        protected void addCriterionForJDBCDate(String condition, Date value1, Date value2, String property) {
-            if (value1 == null || value2 == null) {
-                throw new RuntimeException("Between values for " + property + " cannot be null");
-            }
-            addCriterion(condition, new java.sql.Date(value1.getTime()), new java.sql.Date(value2.getTime()), property);
         }
 
         public Criteria andIdIsNull() {
@@ -683,52 +656,52 @@ public class PcsPrRecommendExample {
         }
 
         public Criteria andMeetingTimeEqualTo(Date value) {
-            addCriterionForJDBCDate("meeting_time =", value, "meetingTime");
+            addCriterion("meeting_time =", value, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeNotEqualTo(Date value) {
-            addCriterionForJDBCDate("meeting_time <>", value, "meetingTime");
+            addCriterion("meeting_time <>", value, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeGreaterThan(Date value) {
-            addCriterionForJDBCDate("meeting_time >", value, "meetingTime");
+            addCriterion("meeting_time >", value, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeGreaterThanOrEqualTo(Date value) {
-            addCriterionForJDBCDate("meeting_time >=", value, "meetingTime");
+            addCriterion("meeting_time >=", value, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeLessThan(Date value) {
-            addCriterionForJDBCDate("meeting_time <", value, "meetingTime");
+            addCriterion("meeting_time <", value, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeLessThanOrEqualTo(Date value) {
-            addCriterionForJDBCDate("meeting_time <=", value, "meetingTime");
+            addCriterion("meeting_time <=", value, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeIn(List<Date> values) {
-            addCriterionForJDBCDate("meeting_time in", values, "meetingTime");
+            addCriterion("meeting_time in", values, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeNotIn(List<Date> values) {
-            addCriterionForJDBCDate("meeting_time not in", values, "meetingTime");
+            addCriterion("meeting_time not in", values, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeBetween(Date value1, Date value2) {
-            addCriterionForJDBCDate("meeting_time between", value1, value2, "meetingTime");
+            addCriterion("meeting_time between", value1, value2, "meetingTime");
             return (Criteria) this;
         }
 
         public Criteria andMeetingTimeNotBetween(Date value1, Date value2) {
-            addCriterionForJDBCDate("meeting_time not between", value1, value2, "meetingTime");
+            addCriterion("meeting_time not between", value1, value2, "meetingTime");
             return (Criteria) this;
         }
 
