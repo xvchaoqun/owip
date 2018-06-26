41ff3b25352d04c7dad3d67ee795b2299068538d
lm_laptop <jxliaom@foxmail.com>
2018-06-26 18:15:52 +0800

    update

--- src/main/java/service/ext/ExtUnitService.java
+++ src/main/java/service/ext/ExtUnitService.java
@@ -5,14 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lm on 2018/6/22.
  */
 @Service
-public class ExtUnitService {
+public class ExtUnitService extends Source {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -22,4 +25,7 @@ public class ExtUnitService {
 
         return records;
     }
+
+    @Override
+    public void update(Map<String, Object> map, ResultSet rs) throws SQLException {}
 }
