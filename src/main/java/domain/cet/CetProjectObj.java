9450ef8a4cb2506004ec41758bfc0ffeb9e39d70
fafa <pwliaom@qq.com>
2019-02-09 11:35:10 +0800

    update

---
+++ src/main/java/domain/cet/CetProjectObj.java
@@ -0,0 +1,259 @@
+package domain.cet;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class CetProjectObj implements Serializable {
+    private Integer id;
+
+    private Integer projectId;
+
+    private Integer userId;
+
+    private Integer traineeTypeId;
+
+    private Boolean isQuit;
+
+    private BigDecimal shouldFinishPeriod;
+
+    private BigDecimal finishPeriod;
+
+    private Boolean isGraduate;
+
+    private String wordWrite;
+
+    private String writeFilePath;
+
+    private String remark;
+
+    private Integer cadreId;
+
+    private String title;
+
+    private Integer typeId;
+
+    private Integer postId;
+
+    private Boolean isOw;
+
+    private Date owGrowTime;
+
+    private Date dpGrowTime;
+
+    private Integer dpTypeId;
+
+    private String proPost;
+
+    private Date lpWorkTime;
+
+    private String mobile;
+
+    private String email;
+
+    private Byte cadreStatus;
+
+    private Integer cadreSortOrder;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getTraineeTypeId() {
+        return traineeTypeId;
+    }
+
+    public void setTraineeTypeId(Integer traineeTypeId) {
+        this.traineeTypeId = traineeTypeId;
+    }
+
+    public Boolean getIsQuit() {
+        return isQuit;
+    }
+
+    public void setIsQuit(Boolean isQuit) {
+        this.isQuit = isQuit;
+    }
+
+    public BigDecimal getShouldFinishPeriod() {
+        return shouldFinishPeriod;
+    }
+
+    public void setShouldFinishPeriod(BigDecimal shouldFinishPeriod) {
+        this.shouldFinishPeriod = shouldFinishPeriod;
+    }
+
+    public BigDecimal getFinishPeriod() {
+        return finishPeriod;
+    }
+
+    public void setFinishPeriod(BigDecimal finishPeriod) {
+        this.finishPeriod = finishPeriod;
+    }
+
+    public Boolean getIsGraduate() {
+        return isGraduate;
+    }
+
+    public void setIsGraduate(Boolean isGraduate) {
+        this.isGraduate = isGraduate;
+    }
+
+    public String getWordWrite() {
+        return wordWrite;
+    }
+
+    public void setWordWrite(String wordWrite) {
+        this.wordWrite = wordWrite == null ? null : wordWrite.trim();
+    }
+
+    public String getWriteFilePath() {
+        return writeFilePath;
+    }
+
+    public void setWriteFilePath(String writeFilePath) {
+        this.writeFilePath = writeFilePath == null ? null : writeFilePath.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Integer getCadreId() {
+        return cadreId;
+    }
+
+    public void setCadreId(Integer cadreId) {
+        this.cadreId = cadreId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public Boolean getIsOw() {
+        return isOw;
+    }
+
+    public void setIsOw(Boolean isOw) {
+        this.isOw = isOw;
+    }
+
+    public Date getOwGrowTime() {
+        return owGrowTime;
+    }
+
+    public void setOwGrowTime(Date owGrowTime) {
+        this.owGrowTime = owGrowTime;
+    }
+
+    public Date getDpGrowTime() {
+        return dpGrowTime;
+    }
+
+    public void setDpGrowTime(Date dpGrowTime) {
+        this.dpGrowTime = dpGrowTime;
+    }
+
+    public Integer getDpTypeId() {
+        return dpTypeId;
+    }
+
+    public void setDpTypeId(Integer dpTypeId) {
+        this.dpTypeId = dpTypeId;
+    }
+
+    public String getProPost() {
+        return proPost;
+    }
+
+    public void setProPost(String proPost) {
+        this.proPost = proPost == null ? null : proPost.trim();
+    }
+
+    public Date getLpWorkTime() {
+        return lpWorkTime;
+    }
+
+    public void setLpWorkTime(Date lpWorkTime) {
+        this.lpWorkTime = lpWorkTime;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Byte getCadreStatus() {
+        return cadreStatus;
+    }
+
+    public void setCadreStatus(Byte cadreStatus) {
+        this.cadreStatus = cadreStatus;
+    }
+
+    public Integer getCadreSortOrder() {
+        return cadreSortOrder;
+    }
+
+    public void setCadreSortOrder(Integer cadreSortOrder) {
+        this.cadreSortOrder = cadreSortOrder;
+    }
+}
