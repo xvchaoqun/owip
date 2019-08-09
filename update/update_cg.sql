f58a2fc9a552b7d158df7accea923d6f123b7322
sws <sws@jixiantech.com>
2019-08-09 17:08:41 +0800

    增加二级党校模块

---
+++ update/update_cg.sql
@@ -0,0 +1,4 @@
+2019-08-08
+-- 修改字段属性 桑文帅
+ALTER TABLE `cg_member` ALTER `user_id` DROP DEFAULT;
+ALTER TABLE `cg_member` CHANGE COLUMN `user_id` `user_id` INT(10) UNSIGNED NULL COMMENT '现任干部' AFTER `unit_post_id`;
