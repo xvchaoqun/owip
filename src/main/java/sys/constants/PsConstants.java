8478738f9cee7fcde05f918c18c692e29669cde9
fafa <pwliaom@qq.com>
2019-08-09 08:21:10 +0800

    update

--- src/main/java/sys/constants/PsConstants.java
+++ src/main/java/sys/constants/PsConstants.java
@@ -3,15 +3,15 @@ package sys.constants;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class PsInfoConstants {
+public class PsConstants {
     //党校管理员类别，1、二级党校管理员 2、院系级党委管理员
     public final static byte PS_ADMIN_TYPE_PARTY = 1;
-    public final static byte PS_ADMIN_TYPE_FACULTY = 2;
+    public final static byte PS_ADMIN_TYPE_UNIT = 2;
     public final static Map<Byte, String> PS_ADMIN_TYPE_MAP = new LinkedHashMap<>();
 
     static {
         PS_ADMIN_TYPE_MAP.put(PS_ADMIN_TYPE_PARTY, "二级党校管理员");
-        PS_ADMIN_TYPE_MAP.put(PS_ADMIN_TYPE_FACULTY, "院系级党委管理员");
+        PS_ADMIN_TYPE_MAP.put(PS_ADMIN_TYPE_UNIT, "院系级党委管理员");
     }
 
     //组织员状态
@@ -19,7 +19,8 @@ public class PsInfoConstants {
     public final static boolean PS_STATUS_NOT_HISTORY = false;//正在使用,未停止记录
 
     //二级党校管理员角色
-    public final static String ROLE_ALLPS_ADMIN = "role_allPs_Admin";//全部的二级党校管理员
-    public final static String ROLE_PS_ADMIN = "role_ps_admin"; //二级党校管理员
+    public final static String ROLE_PS_PARTY = "role_ps_party";//二级党校管理员
+    public final static String ROLE_PS_UNIT = "role_ps_unit";//二级党校管理员（院系）
+    public final static String ROLE_PS_ADMIN = "role_ps_admin"; //二级党校管理员（组织部）
 
 }
