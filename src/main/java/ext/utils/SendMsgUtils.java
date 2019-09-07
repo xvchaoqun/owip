5e3a064a9025a43975136395305b95eac8a3fade
fafa <pwliaom@qq.com>
2019-09-07 10:41:19 +0800

    update

--- src/main/java/ext/utils/SendMsgUtils.java
+++ src/main/java/ext/utils/SendMsgUtils.java
@@ -3,21 +3,13 @@ package ext.utils;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import controller.global.OpException;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.util.HtmlUtils;
 import sys.entity.SendMsgResult;
 import sys.gson.GsonUtils;
+import sys.utils.HttpUtils;
 import sys.utils.PropertiesUtils;
-
-import java.io.IOException;
 
 /**
  * Created by lm on 2017/10/24.
@@ -33,30 +25,17 @@ public class SendMsgUtils {
         String url = PropertiesUtils.getString("shortMsg.url");
         String formStatusData = "{\"mobile\":\"" + mobile + "\", \"content\":\"" + HtmlUtils.htmlUnescape(content) + "\"}";
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httppost = new HttpPost(url);
-        httppost.setEntity(new StringEntity(formStatusData, "UTF-8"));
-        httppost.addHeader("content-type", "application/json");
         try {
-            CloseableHttpResponse res = httpclient.execute(httppost);
-            if (res.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String ret = HttpUtils.doPost(url, formStatusData);
+            JsonObject jsonObject = GsonUtils.toJsonObject(ret);
+            JsonElement errcode = jsonObject.get("errcode");
 
-                String ret = EntityUtils.toString(res.getEntity());
-
-                JsonObject jsonObject = GsonUtils.toJsonObject(ret);
-                JsonElement errcode = jsonObject.get("errcode");
-
-                sendMsgResult.setSuccess((errcode.getAsInt() == 0));
-                sendMsgResult.setMsg(ret);
-
-                return sendMsgResult;
-            }
-        } catch (IOException e) {
-
+            sendMsgResult.setSuccess((errcode.getAsInt() == 0));
+            sendMsgResult.setMsg(ret);
+            return sendMsgResult;
+        } catch (Exception e) {
             logger.error("异常", e);
             throw new OpException("系统错误:" + e.getMessage());
         }
-
-        return null;
     }
 }
