53c3932bfc9476d485776f58fff5885483ed3904
half <liyang@jixiantech.com>
2019-12-09 22:25:20 +0800

    参训人类型和培训对象的导入

--- views/view_cet.sql
+++ views/view_cet.sql
@@ -5,7 +5,7 @@
 DROP VIEW IF EXISTS `cet_trainee_course_view`;
 CREATE ALGORITHM = UNDEFINED VIEW `cet_trainee_course_view` AS
 select cteec.id as id,cteec.trainee_id as trainee_id,cteec.train_course_id as train_course_id,
-cteec.can_quit as can_quit,cteec.is_finished as is_finished,cteec.is_sign as is_sign,cteec.sign_time as sign_time,cteec.is_sign_out as is_sign_out,cteec.sign_out_time as sign_out_time,
+cteec.can_quit as can_quit,cteec.is_finished as is_finished,cteec.sign_time as sign_time,cteec.sign_out_time as sign_out_time,
 cteec.sign_type as sign_type,cteec.remark as remark,cteec.choose_time as choose_time,cteec.choose_user_id as choose_user_id,
 cteec.ip as ip,ctee.train_id as train_id,cpo.project_id as project_id,cpo.trainee_type_id as trainee_type_id,
 cpo.user_id as user_id,ctc.course_id as course_id,cc.period as period,cp.year as year,uv.code as choose_user_code,uv.realname as choose_user_name
