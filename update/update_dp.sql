9f4159b754ea57f86a1c0fd756c8e0148247127c
half <liyang@jixiantech.com>
2019-12-18 18:32:46 +0800

    统站部分使用一次后的修改

--- update/update_dp.sql
+++ update/update_dp.sql
@@ -2,6 +2,10 @@
 
 -- 20191218 ly 统战的view也都更新了 dp_member、dp_pr_cm表更新
 
+REPLACE INTO `sys_resource` (`id`, `is_mobile`, `name`, `remark`, `type`, `menu_css`, `url`, `parent_id`, `parent_ids`, `is_leaf`, `permission`, `role_count`, `count_cache_keys`, `count_cache_roles`, `available`, `sort_order`) VALUES (2606, 0, '人大代表、政协委员', '', 'url', '', '/dp/dpPrCm', 2574, '0/1/2574/', 0, 'dpPrCm:list', NULL, NULL, NULL, 1, 125);
+DELETE FROM `db_owip`.`sys_resource` WHERE  `id`>=2610 and `id`<=2616;
+
+
 INSERT INTO `sys_resource` (`id`, `is_mobile`, `name`, `remark`, `type`, `menu_css`, `url`, `parent_id`, `parent_ids`, `is_leaf`, `permission`, `role_count`, `count_cache_keys`, `count_cache_roles`, `available`, `sort_order`) VALUES (2620, 0, '年度考核', '', 'function', '', NULL, 2574, '0/1/2574/', 0, 'dpEva:list', NULL, NULL, NULL, 1, 120);
 INSERT INTO `sys_resource` (`id`, `is_mobile`, `name`, `remark`, `type`, `menu_css`, `url`, `parent_id`, `parent_ids`, `is_leaf`, `permission`, `role_count`, `count_cache_keys`, `count_cache_roles`, `available`, `sort_order`) VALUES (2621, 0, '年度考核编辑', '', 'function', '', NULL, 2620, '0/1/2574/2620/', 1, 'dpEva:edit', NULL, NULL, NULL, 1, NULL);
 INSERT INTO `sys_resource` (`id`, `is_mobile`, `name`, `remark`, `type`, `menu_css`, `url`, `parent_id`, `parent_ids`, `is_leaf`, `permission`, `role_count`, `count_cache_keys`, `count_cache_roles`, `available`, `sort_order`) VALUES (2622, 0, '年度考核删除', '', 'function', '', NULL, 2620, '0/1/2574/2620/', 1, 'dpEva:del', NULL, NULL, NULL, 1, NULL);
