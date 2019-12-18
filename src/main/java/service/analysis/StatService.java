a984eb89708a713d8b183b1824a8537b880fe62a
fafa <pwliaom@qq.com>
2019-12-18 23:22:01 +0800

    update

--- src/main/java/service/analysis/StatService.java
+++ src/main/java/service/analysis/StatService.java
@@ -151,9 +151,9 @@ public class StatService extends BaseMapper{
 
             for (StatIntBean other : others){
 
-                if (other.getGroupBy()==SystemConstants.GENDER_MALE){
+                if (other.getGroupBy()!=null && other.getGroupBy()==SystemConstants.GENDER_MALE){
                     otherMap.put("男",other.getNum());
-                }else if (other.getGroupBy()==SystemConstants.GENDER_FEMALE){
+                }else if (other.getGroupBy()!=null && other.getGroupBy()==SystemConstants.GENDER_FEMALE){
                     otherMap.put("女",other.getNum());
                 }else{
                     otherMap.put("无数据",other.getNum());
