811fc8fb5d97db8af30b0ce5a596cf33a124554b
half <liyang@jixiantech.com>
2021-02-01 10:41:27 +0800

    信息统计

---
+++ src/main/java/controller/analysis/StatOwInfoController.java
@@ -0,0 +1,11 @@
+package controller.analysis;
+
+import controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/statOwInfo")
+@Controller
+public class StatOwInfoController extends BaseController {
+
+}
---
+++ src/main/java/service/analysis/StatOwInfoService.java
@@ -0,0 +1,10 @@
+package service.analysis;
+
+import org.springframework.stereotype.Service;
+import service.BaseMapper;
+
+@Service
+public class StatOwInfoService extends BaseMapper {
+
+
+}
