c7d8c71355ae3dcd39b3038be2f68bfb34c92308
xvchaoqun <13546114347long?>
2021-02-04 14:05:41 +0800

    整理代码

--- src/main/java/controller/analysis/StatOwInfoController.java
+++ src/main/java/controller/analysis/StatOwInfoController.java
@@ -47,76 +47,19 @@ public class StatOwInfoController extends BaseController {
         int month = DateUtils.getMonth(now);
         modelMap.put("year", year);
         modelMap.put("month", month);
-
-        Map<String, String> masters = new HashMap<>();
-        Map<String, String> doctors = new HashMap<>();
-        String schoolName = CmTag.getSysConfig().getSchoolName();
-        modelMap.put("schoolName", schoolName);
         DecimalFormat df = new DecimalFormat("0.00");
 
+        String schoolName = CmTag.getSysConfig().getSchoolName();
+        modelMap.put("schoolName", schoolName);
+
         if (cls == 0) {
-            // 全校研究生总数
-            SysUserExample example = new SysUserExample();
-            example.createCriteria().andTypeEqualTo(SystemConstants.SYNC_TYPE_YJS).andLockedEqualTo(false);
-            int countYjs = (int) sysUserMapper.countByExample(example);
-
-            // 预备党员
-            List<StatByteBean> preparedMembers = statOwInfoMapper.member_groupByType(MemberConstants.MEMBER_POLITICAL_STATUS_GROW, null, null, null, null);
-            // 正式党员
-            List<StatByteBean> formalMembers = statOwInfoMapper.member_groupByType(MemberConstants.MEMBER_POLITICAL_STATUS_POSITIVE, null, null, null, null);
-            //申请入党人员
-            List<StatByteBean> applyJoin = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_INIT, null, null, null);
-            List<StatByteBean> passJoin = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_PASS, null, null, null);
-            // 入党积极分子
-            List<StatByteBean> countActivists = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_ACTIVE, null, null, null);
-            // 发展对象
-            List<StatByteBean> countDevelopment = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_CANDIDATE, null, null, null);
-            // 封装数据
-            List<Map<String, String>> data = statOwInfoService.encapsulationData(preparedMembers, formalMembers, applyJoin, passJoin, countActivists, countDevelopment);
-            masters = data.get(0);
-            doctors = data.get(1);
-
-            //硕士和博士研究生总数
-            StudentInfoExample studentInfoExample = new StudentInfoExample();
-            studentInfoExample.createCriteria().andStudentLevelEqualTo(SystemConstants.STUDENT_TYPE_SS);
-            int countMasters = (int) studentInfoMapper.countByExample(studentInfoExample);
-            StudentInfoExample studentInfoExample2 = new StudentInfoExample();
-            studentInfoExample2.createCriteria().andStudentLevelEqualTo(SystemConstants.STUDENT_TYPE_BS);
-            int countDoctors = (int) studentInfoMapper.countByExample(studentInfoExample2);
-
-            //硕士研究生总数
-            masters.put("total", String.valueOf(countMasters));
-            //博士研究生总数
-            doctors.put("total", String.valueOf(countDoctors));
-
-            //硕士生党员占比
-            BigDecimal num1 = new BigDecimal(masters.get("partyMembersCount"));
-            BigDecimal num2 = new BigDecimal(countMasters);
-            BigDecimal masterPercent = num1.divide(num2,4,BigDecimal.ROUND_HALF_UP);
-
-            //博士生党员占比
-            num1 = new BigDecimal(doctors.get("partyMembersCount"));
-            num2 = new BigDecimal(countDoctors);
-            BigDecimal doctorPercent = num1.divide(num2,4,BigDecimal.ROUND_HALF_UP);
-
-            modelMap.put("total", countYjs);
-            //研究生占比
-            Integer mpmc = masters.get("partyMembersCount") == null ? 0 : Integer.valueOf(masters.get("partyMembersCount"));
-            Integer dpmc = doctors.get("partyMembersCount") == null ? 0 : Integer.valueOf(doctors.get("partyMembersCount"));
-            Integer total = modelMap.get("total") == null ? 0 : (Integer) modelMap.get("total");
-            num1 = new BigDecimal(mpmc + dpmc);
-            num2 = new BigDecimal(total);
-            BigDecimal percent = num1.divide(num2,4,BigDecimal.ROUND_HALF_UP);
-
-            masters.put("masterPercent", df.format(masterPercent.doubleValue() * 100) + "%");
-            doctors.put("doctorPercent", df.format(doctorPercent.doubleValue() * 100) + "%");
-
-            modelMap.put("percent", df.format(percent.doubleValue() * 100) + "%");
-            modelMap.put("masters", masters);
-            modelMap.put("doctors", doctors);
-
+            modelMap = statOwInfoService.getYjsInfo(modelMap, df);
             if (export == 2) {
-                XSSFWorkbook wb = statOwInfoService.statOnInfoExport(modelMap, masterPercent, doctorPercent);
+                Map<String, String> masters = (Map<String, String>) modelMap.get("masters");
+                Map<String, String> doctors = (Map<String, String>) modelMap.get("doctors");
+                String masterPercent = masters.get("masterPercent");
+                String doctorPercent = doctors.get("doctorPercent");
+                XSSFWorkbook wb = statOwInfoService.statOnInfoExport(modelMap);
                 String filename = String.format("%s研究生队伍党员信息分析.xlsx", schoolName);
                 ExportHelper.output(wb, filename, response);
                 return null;
@@ -134,80 +77,10 @@ public class StatOwInfoController extends BaseController {
             columns.add("党员占比");
             modelMap.put("columns", columns);
 
-            List<Map<String, String>> data = new ArrayList<>();
-            // 二级党组织名称
-
-            List<Party> partyNames = statOwInfoMapper.getSecondPartyName();
-            for (Party p: partyNames) {
-                //申请入党人员
-                List<StatByteBean> applyJoin = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_INIT, null, p.getId(), null);
-                List<StatByteBean> passJoin = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_PASS, null, p.getId(), null);
-                // 入党积极分子
-                List<StatByteBean> countActivists = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_ACTIVE, null, p.getId(), null);
-                // 发展对象
-                List<StatByteBean> countDevelopment = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_CANDIDATE, null, p.getId(), null);
-                // 预备党员
-                List<StatByteBean> preparedMembers = statOwInfoMapper.member_groupByType(MemberConstants.MEMBER_POLITICAL_STATUS_GROW, p.getId(), null, null, null);
-                // 正式党员
-                List<StatByteBean> formalMembers = statOwInfoMapper.member_groupByType(MemberConstants.MEMBER_POLITICAL_STATUS_POSITIVE, p.getId(), null, null, null);
-                // 封装数据
-                List<Map<String, String>> result = statOwInfoService.encapsulationData(preparedMembers, formalMembers, applyJoin, passJoin, countActivists, countDevelopment);
-                masters = result.get(0);
-                doctors = result.get(1);
-
-                masters.put("identity", "masters");
-                doctors.put("identity", "doctors");
-                // 普通学生
-                int general = 0;
-                masters.put("generalCount", String.valueOf(general));
-                doctors.put("generalCount", String.valueOf(general));
-                //硕士研究生合计
-                Integer applyTotal = masters.get("applyTotal") == null ? 0 : Integer.valueOf(masters.get("applyTotal"));
-                Integer activityTotal = masters.get("activityTotal") == null ? 0 : Integer.valueOf(masters.get("activityTotal"));
-                Integer developTotal = masters.get("developTotal") == null ? 0 : Integer.valueOf(masters.get("developTotal"));
-                Integer formalMember = masters.get("formalMembers") == null ? 0 : Integer.valueOf(masters.get("formalMembers"));
-                Integer preparedMember = masters.get("preparedMembers") == null ? 0 : Integer.valueOf(masters.get("preparedMembers"));
-                Integer masterTotal = applyTotal + activityTotal + developTotal + formalMember + preparedMember +
-                                      (masters.get("generalCount") == null ? 0 : Integer.valueOf(masters.get("generalCount")));
-                masters.put("total", masterTotal.toString());
-                //培养情况占比 (入党申请人+入党积极分子+发展对象)/合计
-                BigDecimal num1 = new BigDecimal(applyTotal + activityTotal + developTotal);
-                BigDecimal num2 = new BigDecimal(masterTotal);
-                BigDecimal masterScale = (num1.compareTo(BigDecimal.ZERO) == 0 || num2.compareTo(BigDecimal.ZERO) == 0 ? new BigDecimal(0) : num1.divide(num2,4,BigDecimal.ROUND_HALF_UP));
-                //党员占比 (正式党员+预备党员)/合计
-                num1 = new BigDecimal(formalMember + preparedMember);
-                num2 = new BigDecimal(masterTotal);
-                BigDecimal masterPercent = (num1.compareTo(BigDecimal.ZERO) == 0 || num2.compareTo(BigDecimal.ZERO) == 0 ? new BigDecimal(0) : num1.divide(num2,4,BigDecimal.ROUND_HALF_UP));
-
-                //博士研究生合计
-                applyTotal = doctors.get("applyTotal") == null ? 0 : Integer.valueOf(doctors.get("applyTotal"));
-                activityTotal = doctors.get("activityTotal") == null ? 0 : Integer.valueOf(doctors.get("activityTotal"));
-                developTotal = doctors.get("developTotal") == null ? 0 : Integer.valueOf(doctors.get("developTotal"));
-                formalMember = doctors.get("formalMembers") == null ? 0 : Integer.valueOf(doctors.get("formalMembers"));
-                preparedMember = doctors.get("preparedMembers") == null ? 0 : Integer.valueOf(doctors.get("preparedMembers"));
-                Integer doctorTotal = applyTotal + activityTotal + developTotal + formalMember + preparedMember +
-                        (doctors.get("generalCount") == null ? 0 : Integer.valueOf(doctors.get("generalCount")));
-                doctors.put("total", doctorTotal.toString());
-                //培养情况占比 (入党申请人+入党积极分子+发展对象)/合计
-                num1 = new BigDecimal(applyTotal + activityTotal + developTotal);
-                num2 = new BigDecimal(doctorTotal);
-                BigDecimal doctorScale = (num1.compareTo(BigDecimal.ZERO) == 0 || num2.compareTo(BigDecimal.ZERO) == 0 ? new BigDecimal(0) : num1.divide(num2,4,BigDecimal.ROUND_HALF_UP));
-                //党员占比 (正式党员+预备党员)/合计
-                num1 = new BigDecimal(formalMember + preparedMember);
-                num2 = new BigDecimal(doctorTotal);
-                BigDecimal doctorPercent = (num1.compareTo(BigDecimal.ZERO) == 0 || num2.compareTo(BigDecimal.ZERO) == 0 ? new BigDecimal(0) : num1.divide(num2,4,BigDecimal.ROUND_HALF_UP));
-
-                masters.put("masterScale", df.format(masterScale.doubleValue() * 100) + "%");
-                doctors.put("doctorScale", df.format(doctorScale.doubleValue() * 100) + "%");
-                masters.put("masterPercent", df.format(masterPercent.doubleValue() * 100) + "%");
-                doctors.put("doctorPercent", df.format(doctorPercent.doubleValue() * 100) + "%");
-                masters.put("partyName", p.getShortName());
-                data.add(masters);
-                data.add(doctors);
-            }
+            List<Map<String, String>> data = statOwInfoService.getYjsPartyInfo(df);
             modelMap.put("data", data);
             if (export == 2) {
-                XSSFWorkbook wb = statOwInfoService.statOnPartyInfoExport(modelMap, partyNames);
+                XSSFWorkbook wb = statOwInfoService.statOnPartyInfoExport(modelMap);
                 String filename = String.format("各二级党组织研究生队伍党员信息分析.xlsx");
                 ExportHelper.output(wb, filename, response);
                 return null;
--- src/main/java/service/analysis/StatOwInfoService.java
+++ src/main/java/service/analysis/StatOwInfoService.java
@@ -2,12 +2,18 @@ package service.analysis;
 
 import bean.StatByteBean;
 import domain.party.Party;
+import domain.sys.StudentInfoExample;
+import domain.sys.SysUserExample;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.xssf.usermodel.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.ResourceUtils;
+import persistence.analysis.StatOwInfoMapper;
 import service.BaseMapper;
+import sys.constants.MemberConstants;
+import sys.constants.OwConstants;
 import sys.constants.SystemConstants;
 import sys.tags.CmTag;
 import java.io.FileInputStream;
@@ -15,6 +21,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,8 +29,10 @@ import java.util.Map;
 
 @Service
 public class StatOwInfoService extends BaseMapper {
+    @Autowired
+    private StatOwInfoMapper statOwInfoMapper;
 
-    public XSSFWorkbook statOnInfoExport(ModelMap modelMap, BigDecimal masterPercent, BigDecimal doctorPercent) {
+    public XSSFWorkbook statOnInfoExport(ModelMap modelMap) {
         InputStream is = null;
         try {
             int startRow = 5;
@@ -236,7 +245,7 @@ public class StatOwInfoService extends BaseMapper {
     }
 
 
-    public XSSFWorkbook statOnPartyInfoExport(ModelMap modelMap, List<Party> partyNames) {
+    public XSSFWorkbook statOnPartyInfoExport(ModelMap modelMap) {
         InputStream is = null;
         try {
             int startRow = 4;
@@ -280,7 +289,6 @@ public class StatOwInfoService extends BaseMapper {
             font2.setFontName("宋体");
             style2.setFont(font2);// 把字体应用到当前的样式
             style.setFont(font2);
-//            cell.setCellStyle(style2);
 
             for (int i = 0; i < data.size(); i++) {
                 Map<String, String> map = data.get(i);
@@ -354,4 +362,141 @@ public class StatOwInfoService extends BaseMapper {
         return null;
     }
 
+    public static BigDecimal calculateDivide(Integer number1, Integer number2) {
+        BigDecimal num1 = new BigDecimal(number1);
+        BigDecimal num2 = new BigDecimal(number2);
+        BigDecimal result = (num1.compareTo(BigDecimal.ZERO) == 0 || num2.compareTo(BigDecimal.ZERO) == 0 ? new BigDecimal(0) : num1.divide(num2,4,BigDecimal.ROUND_HALF_UP));
+        return result;
+    }
+
+    public ModelMap getYjsInfo(ModelMap modelMap, DecimalFormat df) {
+        Map<String, String> masters = new HashMap<>();
+        Map<String, String> doctors = new HashMap<>();
+
+        // 全校研究生总数
+        SysUserExample example = new SysUserExample();
+        example.createCriteria().andTypeEqualTo(SystemConstants.SYNC_TYPE_YJS).andLockedEqualTo(false);
+        int countYjs = (int) sysUserMapper.countByExample(example);
+
+        // 预备党员
+        List<StatByteBean> preparedMembers = statOwInfoMapper.member_groupByType(MemberConstants.MEMBER_POLITICAL_STATUS_GROW, null, null, null, null);
+        // 正式党员
+        List<StatByteBean> formalMembers = statOwInfoMapper.member_groupByType(MemberConstants.MEMBER_POLITICAL_STATUS_POSITIVE, null, null, null, null);
+        //申请入党人员
+        List<StatByteBean> applyJoin = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_INIT, null, null, null);
+        List<StatByteBean> passJoin = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_PASS, null, null, null);
+        // 入党积极分子
+        List<StatByteBean> countActivists = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_ACTIVE, null, null, null);
+        // 发展对象
+        List<StatByteBean> countDevelopment = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_CANDIDATE, null, null, null);
+        // 封装数据
+        List<Map<String, String>> data = encapsulationData(preparedMembers, formalMembers, applyJoin, passJoin, countActivists, countDevelopment);
+        masters = data.get(0);
+        doctors = data.get(1);
+
+        //硕士和博士研究生总数
+        StudentInfoExample studentInfoExample = new StudentInfoExample();
+        studentInfoExample.createCriteria().andStudentLevelEqualTo(SystemConstants.STUDENT_TYPE_SS);
+        int countMasters = (int) studentInfoMapper.countByExample(studentInfoExample);
+        StudentInfoExample studentInfoExample2 = new StudentInfoExample();
+        studentInfoExample2.createCriteria().andStudentLevelEqualTo(SystemConstants.STUDENT_TYPE_BS);
+        int countDoctors = (int) studentInfoMapper.countByExample(studentInfoExample2);
+
+        //硕士研究生总数
+        masters.put("total", String.valueOf(countMasters));
+        //博士研究生总数
+        doctors.put("total", String.valueOf(countDoctors));
+
+        //硕士生党员占比
+        BigDecimal masterPercent = calculateDivide(Integer.valueOf(masters.get("partyMembersCount")), countMasters);
+
+        //博士生党员占比
+        BigDecimal doctorPercent = calculateDivide(Integer.valueOf(doctors.get("partyMembersCount")), countDoctors);
+
+        modelMap.put("total", countYjs);
+        //研究生占比
+        Integer mpmc = masters.get("partyMembersCount") == null ? 0 : Integer.valueOf(masters.get("partyMembersCount"));
+        Integer dpmc = doctors.get("partyMembersCount") == null ? 0 : Integer.valueOf(doctors.get("partyMembersCount"));
+        Integer total = modelMap.get("total") == null ? 0 : (Integer) modelMap.get("total");
+
+        BigDecimal percent = calculateDivide(mpmc + dpmc, total);
+
+        masters.put("masterPercent", df.format(masterPercent.doubleValue() * 100) + "%");
+        doctors.put("doctorPercent", df.format(doctorPercent.doubleValue() * 100) + "%");
+
+        modelMap.put("percent", df.format(percent.doubleValue() * 100) + "%");
+        modelMap.put("masters", masters);
+        modelMap.put("doctors", doctors);
+        return modelMap;
+    }
+
+    public List<Map<String, String>> getYjsPartyInfo(DecimalFormat df) {
+        Map<String, String> masters = new HashMap<>();
+        Map<String, String> doctors = new HashMap<>();
+        List<Map<String, String>> data = new ArrayList<>();
+        // 二级党组织名称
+        List<Party> partyNames = statOwInfoMapper.getSecondPartyName();
+        for (Party p: partyNames) {
+            //申请入党人员
+            List<StatByteBean> applyJoin = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_INIT, null, p.getId(), null);
+            List<StatByteBean> passJoin = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_PASS, null, p.getId(), null);
+            // 入党积极分子
+            List<StatByteBean> countActivists = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_ACTIVE, null, p.getId(), null);
+            // 发展对象
+            List<StatByteBean> countDevelopment = statOwInfoMapper.memberApply_groupByLevel(OwConstants.OW_APPLY_STAGE_CANDIDATE, null, p.getId(), null);
+            // 预备党员
+            List<StatByteBean> preparedMembers = statOwInfoMapper.member_groupByType(MemberConstants.MEMBER_POLITICAL_STATUS_GROW, p.getId(), null, null, null);
+            // 正式党员
+            List<StatByteBean> formalMembers = statOwInfoMapper.member_groupByType(MemberConstants.MEMBER_POLITICAL_STATUS_POSITIVE, p.getId(), null, null, null);
+            // 封装数据
+            List<Map<String, String>> result = encapsulationData(preparedMembers, formalMembers, applyJoin, passJoin, countActivists, countDevelopment);
+            masters = result.get(0);
+            doctors = result.get(1);
+
+            masters.put("identity", "masters");
+            doctors.put("identity", "doctors");
+            // 普通学生
+            int general = 0;
+            masters.put("generalCount", String.valueOf(general));
+            doctors.put("generalCount", String.valueOf(general));
+            //硕士研究生合计
+            Integer applyTotal = masters.get("applyTotal") == null ? 0 : Integer.valueOf(masters.get("applyTotal"));
+            Integer activityTotal = masters.get("activityTotal") == null ? 0 : Integer.valueOf(masters.get("activityTotal"));
+            Integer developTotal = masters.get("developTotal") == null ? 0 : Integer.valueOf(masters.get("developTotal"));
+            Integer formalMember = masters.get("formalMembers") == null ? 0 : Integer.valueOf(masters.get("formalMembers"));
+            Integer preparedMember = masters.get("preparedMembers") == null ? 0 : Integer.valueOf(masters.get("preparedMembers"));
+            Integer masterTotal = applyTotal + activityTotal + developTotal + formalMember + preparedMember +
+                    (masters.get("generalCount") == null ? 0 : Integer.valueOf(masters.get("generalCount")));
+            masters.put("total", masterTotal.toString());
+            //培养情况占比 (入党申请人+入党积极分子+发展对象)/合计
+            BigDecimal masterScale = calculateDivide(applyTotal + activityTotal + developTotal, masterTotal);
+
+            //党员占比 (正式党员+预备党员)/合计
+            BigDecimal masterPercent = calculateDivide(formalMember + preparedMember, masterTotal);
+
+            //博士研究生合计
+            applyTotal = doctors.get("applyTotal") == null ? 0 : Integer.valueOf(doctors.get("applyTotal"));
+            activityTotal = doctors.get("activityTotal") == null ? 0 : Integer.valueOf(doctors.get("activityTotal"));
+            developTotal = doctors.get("developTotal") == null ? 0 : Integer.valueOf(doctors.get("developTotal"));
+            formalMember = doctors.get("formalMembers") == null ? 0 : Integer.valueOf(doctors.get("formalMembers"));
+            preparedMember = doctors.get("preparedMembers") == null ? 0 : Integer.valueOf(doctors.get("preparedMembers"));
+            Integer doctorTotal = applyTotal + activityTotal + developTotal + formalMember + preparedMember +
+                    (doctors.get("generalCount") == null ? 0 : Integer.valueOf(doctors.get("generalCount")));
+            doctors.put("total", doctorTotal.toString());
+            //培养情况占比 (入党申请人+入党积极分子+发展对象)/合计
+            BigDecimal doctorScale = calculateDivide(applyTotal + activityTotal + developTotal, doctorTotal);
+
+            //党员占比 (正式党员+预备党员)/合计
+            BigDecimal doctorPercent = calculateDivide(formalMember + preparedMember, doctorTotal);
+
+            masters.put("masterScale", df.format(masterScale.doubleValue() * 100) + "%");
+            doctors.put("doctorScale", df.format(doctorScale.doubleValue() * 100) + "%");
+            masters.put("masterPercent", df.format(masterPercent.doubleValue() * 100) + "%");
+            doctors.put("doctorPercent", df.format(doctorPercent.doubleValue() * 100) + "%");
+            masters.put("partyName", p.getShortName());
+            data.add(masters);
+            data.add(doctors);
+        }
+        return data;
+    }
 }
