6485986368969f741afd06d41c01063e5fefbcfe
liaomin <liaomin@jixiantech.com>
2021-05-20 11:18:44 +0800

    update

--- views/release/view_ext.sql
+++ views/release/view_ext.sql
@@ -114,11 +114,11 @@ left join ow_branch ob on om.branch_id=ob.id;
 -- 京外已转出视图
 DROP VIEW IF EXISTS `ext_member_out_view`;
 CREATE ALGORITHM = UNDEFINED VIEW `ext_member_out_view`
-AS SELECT u.code, u.realname, u.gender, m.political_status,u.idcard, u.nation, u.birth, o.phone, o.to_title, o.to_unit, o.from_unit, o.from_address, o.from_phone,
+AS SELECT u.code, o.realname, o.gender, o.political_status,o.idcard, o.nation, o.age,
+          o.phone, o.to_title, o.to_unit, o.from_unit, o.from_address, o.from_phone,
 o.from_fax, o.from_post_code, o.pay_time, o.valid_days, o.handle_time, o.has_receipt, o.apply_time,
 o.is_self_print as self_print, o.is_self_print_count as self_print_count from ow_member_out o
-left join sys_user_view u on o.user_id = u.user_id
-left join ow_member m on m.user_id = u.user_id
+left join sys_user  u on o.user_id = u.id
 LEFT JOIN base_meta_type bmt ON o.`type`=bmt.id
 WHERE o.STATUS=2 AND bmt.name='京外' ;
 
